4b8f1d8a9f31fec2c016ad2c3d4e1af3b5c36e00
kwangbi <kwangbi@naver.com>
2021-01-05 21:49:32 +0900

    Initial commit

---
+++ src/main/java/com/yang/kwang/KwangApplication.java
@@ -0,0 +1,13 @@
+package com.yang.kwang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KwangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yang/kwang/KwangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yang.kwang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KwangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
